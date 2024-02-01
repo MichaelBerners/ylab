9170d0956cf4b47e196b22ef937984115b116e38
mihail <mihail_belonogov@bk.ru>
2024-02-02 01:20:54 +0300

    add docker-compose, db connection, migration

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -2,8 +2,17 @@ import domain.exception.CounterReadingsException;
 import domain.exception.UserException;
 import in.controller.CounterReadingsController;
 import in.controller.UserController;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.ClassLoaderResourceAccessor;
 import lombok.SneakyThrows;
+import util.ConnectionManager;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 /**
@@ -13,6 +22,18 @@ import java.util.Scanner;
 public class Runner {
     @SneakyThrows
     public static void main(String[] args) {
+        try {
+            Connection connection = ConnectionManager.getConnection();
+            Database database =
+                    DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(connection));
+            Liquibase liquibase =
+                    new Liquibase("db/changelog/db.changelog.xml", new ClassLoaderResourceAccessor(), database);
+            liquibase.update();
+            System.out.println("Migration is completed successfully");
+        }
+        catch (LiquibaseException e) {
+            System.out.println("SQL Exception in migration " + e.getMessage());
+        }
         int number = 0;
 
         do {
---
+++ src/main/java/util/ConnectionManager.java
@@ -0,0 +1,28 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionManager {
+    private static final String URL_KEY = "db.url";
+    private static final String USER_NAME_KEY = "db.userName";
+    private static final String PASSWORD_KEY = "db.password";
+
+    private ConnectionManager(){};
+
+    public static Connection getConnection() {
+        try {
+            String s = PropertiesUtil.get(URL_KEY);
+            String s1 = PropertiesUtil.get(USER_NAME_KEY);
+            String s2 = PropertiesUtil.get(PASSWORD_KEY);
+            System.out.println();
+            Connection connection = DriverManager.getConnection(s, s1, s2);
+
+            return connection;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
